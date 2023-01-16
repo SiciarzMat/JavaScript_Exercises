a8903735220107f9256632277a0b21c8e12cb744
Mateusz Siciarz <siciarz.mat@gmail.com>
2023-01-16 19:16:50 +0100

    task 9.1 - fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,6 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 let colorChange = null;
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
