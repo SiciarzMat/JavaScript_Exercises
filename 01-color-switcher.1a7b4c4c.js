290051cde15bcf4d37527fef7cfa3d7f7473c83f
SiciarzMat <61201511+SiciarzMat@users.noreply.github.com>
2023-01-16 18:13:03 +0000

    Deploying to gh-pages from @ SiciarzMat/goit-js-hw-09@04a03eb2eadbffdfee6a9a23694c3c30131be84c 🚀

--- 01-color-switcher.1a7b4c4c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;t.addEventListener("click",(function(){start.disabled=!0,e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){start.disabled=!1,clearInterval(e)}))}();
-//# sourceMappingURL=01-color-switcher.1a7b4c4c.js.map
