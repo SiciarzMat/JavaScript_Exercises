1f1c171397622440c177d452a6f513af13b52c00
Mateusz Siciarz <siciarz.mat@gmail.com>
2023-01-16 18:52:54 +0100

    9.1 fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,9 @@
 const startBtn = document.querySelectorAll('button')[0];
 const stopBtn = document.querySelectorAll('button')[1];
 let colorChange = null;
-getRandomHexColor = () => {
+function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+}
 
 startBtn.addEventListener('click', () => {
   start.disabled = true;
